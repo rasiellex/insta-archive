aad548250c01f28129a9fb7be4df41f48e6f6bcb
michellereiners <michelle.reiners@smail.th-koeln.de>
2024-06-16 14:20:47 +0200

    [feat] add script download with instagrapi

--- story_download.py
+++ story_download.py
@@ -6,6 +6,8 @@ import instaloader
 import pytz
 import yaml
 from loguru import logger
+from instagrapi import Client
+import os
 
 from helper_functions import send_to_discord_webhook
 
@@ -17,47 +19,48 @@ if __name__ == "__main__":
     with open('config.yml', 'r') as file:
         config = yaml.safe_load(file)
 
-    user = config["INSTA"]["USER"]
+    users = config["INSTA"]["USER"]
     pw = config["INSTA"]["PASSWORD"]
     data_path = config["DATA_PATH"]
-    instagram_profile = config["INSTAGRAM_PROFILE"]
+    instagram_profile = "gryffin"
     user_timezone = config["USER_TIMEZONE"]
     webhook_discord = config["DISCORD"]["WEBHOOK_INSTA_DOWNLOAD"]
     webhook_discord_alert = config["DISCORD"]["WEBHOOK_ALERT"]
 
     try:
-        loader = instaloader.Instaloader(
-            download_video_thumbnails=False
-        )
+        user = random.choice(list(users.values()))
+
+        cl = Client()
+
+        # adds a random delay between 1 and 3 seconds after each request
+        cl.delay_range = [3, 10]
         try:
             logger.info("Try to login via session file.")
-            loader.load_session_from_file(username=user, filename="session_instaloader")
+            cl.load_settings(f"session_instagrapi_{user}")
+            cl.login(user, pw)
             logger.info(f"Successfully logged in to account: {user} via session file.")
-            loader.test_login()
         except:
             logger.info("Login via session file failed. Session file expired.")
             try:
                 logger.info("Login in via credentials and save session file.")
-                loader.login(user=user, passwd=pw)
-                loader.save_session_to_file("session_instaloader")
-                loader.test_login()
+                cl.login(user, pw)
+                cl.dump_settings(f"session_instagrapi_{user}")
                 logger.info(f"Successfully logged in to account: {user} via credentials and saved session file to disk.")
             except Exception as e:
-                raise
+                logger.exception(e)
 
-        # Retrieve the profile metadata
-        profile = instaloader.Profile.from_username(loader.context, instagram_profile)
+            user_info = cl.user_info_by_username_v1('gryffin')  # user_info_by_username_v1
 
-        stories = loader.get_stories(userids=[profile.userid])
-
-        downloaded_items = 0
-
-        for story in stories:
-
-            num_stories = story.itemcount
+            user_id = user_info.pk
+            user_stories = cl.user_stories(user_id=user_id)
+            num_stories = len(user_stories)
             logger.info(f"Number of stories found: {num_stories}")
-            for item in story.get_items():
-                local_timestamp = item.date_utc
+
+            downloaded_items = 0
+
+            for story in user_stories:
+                local_timestamp = story.taken_at
+                local_timestamp = local_timestamp.replace(tzinfo=None)
 
                 # Convert it to GMT-6
                 denver_timezone = pytz.timezone('US/Mountain')
@@ -66,19 +69,32 @@ if __name__ == "__main__":
                 date = local_timestamp.strftime('%Y-%m-%d-%H-%M-%S')
                 dir_name = local_timestamp.strftime('%Y-%m-%d')
 
-                loader.dirname_pattern = f"{data_path}{dir_name}/"
-                loader.filename_pattern = f'{date}'
-                loader.download_storyitem(item, target='')
+                media_type = story.media_type
+                if media_type == 1:
+                    extension = ".jpg"
+                else:
+                    extension = ".mp4"
 
+                data_path = "test/"
+                dirname = f"{data_path}{dir_name}/"
+                filename = f'{date}'
+
+                if not os.path.exists(dirname):
+                    os.makedirs(dirname)
+
+                if os.path.isfile(dirname + filename + extension):
+                    continue
+
+                cl.story_download(story.pk, filename=filename, folder=dirname)
                 downloaded_items += 1
                 logger.info(f"Download progress: {downloaded_items} of {num_stories} items downloaded.")
-                delay = random.randint(4, 10)
-                time.sleep(delay)
+
             logger.success(f"Successfully downloaded {num_stories} stories.")
 
-        if downloaded_items == 0:
-            logger.info("No stories found.")
-        logger.info("Finished process: Download Instagram stories. End script.")
+
+            if downloaded_items == 0:
+                logger.info("No stories found.")
+            logger.info("Finished process: Download Instagram stories. End script.")
 
     except Exception as e:
         logger.info("Script failed.")
